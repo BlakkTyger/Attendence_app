39451f1d4a9c866ed7ba873f58f9a2add7596134
BlakkTyger <himans23@iitk.ac.in>
2024-05-20 22:53:34 +0530

    some red. frontend

--- Attendence_app/Attendence_app/settings.py
+++ Attendence_app/Attendence_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'attendence',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,6 @@ import os
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+LOGIN_REDIRECT_URL = "attendence:profile"
+LOGOUT_REDIRECT_URL = "attendence:login"
--- Attendence_app/attendence/views.py
+++ Attendence_app/attendence/views.py
@@ -1,20 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+
 
 def sign_up(request):
     return render(request, 'signup.html')
 
+
 def log_in(request):
+    
     return render(request, 'login.html')
 
+# logout page
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
 def profile(request):
     return render(request, 'profile.html')
 
+@login_required
 def mark_att(request):
     return render(request, 'mark-att.html')
 
+@login_required
 def att_marked(request):
     return render(request, 'att-marked.html')
 
+@login_required
 def check_att(request):
     return render(request, 'check-att.html')
