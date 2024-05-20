84b3f8888720812f97c7f5be50731116559acc1d
BlakkTyger <himans23@iitk.ac.in>
2024-05-20 13:23:55 +0530

    endpointa added

--- Attendence_app/attendence/urls.py
+++ Attendence_app/attendence/urls.py
@@ -2,5 +2,10 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name = 'home')
+    path('signup/', views.sign_up, name = 'sign_up'),
+    path('login/', views.log_in, name = 'log_in'),
+    path('user-profile/', views.profile, name = 'profile'),
+    path('mark-attendence/', views.mark_att, name = 'mark_att'),
+    path('attendence-successful', views.att_marked, name = 'att_marked'),
+    path('check-attendence/', views.check_att, name = 'check_att'),
 ]
--- Attendence_app/attendence/views.py
+++ Attendence_app/attendence/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("HelloWorld")
+def sign_up(request):
+    return render(request, 'signup.html')
+
+def log_in(request):
+    return render(request, 'login.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def mark_att(request):
+    return render(request, 'mark-att.html')
+
+def att_marked(request):
+    return render(request, 'att-marked.html')
+
+def check_att(request):
+    return render(request, 'check-att.html')
